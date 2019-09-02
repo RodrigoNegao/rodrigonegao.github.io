30903efadb2bbfae8e8e0ba10fa0ad487704fd83
Rodrigo da Cunha Pereira <RodrigoNegao@users.noreply.github.com>
2019-09-02 18:06:51 -0300

    type

---
+++ js/typing.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".typing" , {
+    strings: ["FrontEnd",
+     "BackEnd", "Full Stack"],
+    // smartBackspace: true Default value
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: false,
+  });
