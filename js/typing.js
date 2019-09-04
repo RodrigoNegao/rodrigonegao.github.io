fdb77a7970fd1d5d9d3224b5bd3eaac9c783daf8
Rodrigo da Cunha Pereira <RodrigoNegao@users.noreply.github.com>
2019-09-04 16:59:08 -0300

    refactoring

--- js/typing.js
+++ js/typing.js
@@ -2,7 +2,7 @@ var typed = new Typed(".typing" , {
     strings: ["FrontEnd",
      "BackEnd", "Full Stack"],
     // smartBackspace: true Default value
-    typeSpeed: 60,
-    backSpeed: 50,
+    typeSpeed: 70,
+    backSpeed: 60,
     loop: false,
   });
